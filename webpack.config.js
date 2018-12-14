30e78a53c45b653f2a35625a62d55843bc6308e0
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2018-12-14 15:28:24 -0800

    start: Tutorial of Building a Website with React and ASPNET Core
    Completing setup of Webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const APP_DIR = path.resolve(__dirname, 'src');
+const PUBLIC_DIR = path.resolve(__dirname, 'public');
+
+const config = {
+    entry: APP_DIR + '/HelloWorld.js',
+    devServer: {
+        contentBase: PUBLIC_DIR,
+        port: 9000
+    },
+    output: {
+        path: PUBLIC_DIR,
+        filename: 'bundle.js',
+    },
+};
+
+module.exports = config;
