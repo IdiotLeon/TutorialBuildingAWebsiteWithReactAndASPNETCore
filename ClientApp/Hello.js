f810d3ec3b6181ed45dbe38027f84cba06ee1c06
Leon <yanglyu.leon.7@gmail.com>
2018-12-14 17:43:01 -0800

    fea: Setting up React

---
+++ ClientApp/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>Hello!!!</div>
+        );
+    }
+}
+
+Hello.propType = {};
+Hello.defaultProps = {};
+
+export default Hello;
